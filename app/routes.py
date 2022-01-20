83c11f2d493d6e5b9cdcd722b640b27042aa5e03
TheOafidian <tim.van.rillaer@hotmail.com>
2022-01-20 13:39:43 +0100

    Completed part 3 of tutorial

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, flash, redirect
+from flask import render_template, flash, redirect, url_for
 from app import app
 from app.forms import LoginForm
 
@@ -15,5 +15,5 @@ def login():
         flash("Login requested for user {}, remember_me={}".format(
             form.username.data, form.remember_me.data
         ))
-        return redirect('/index')
+        return redirect(url_for('index'))
     return render_template("auth/login.html", form=form)
