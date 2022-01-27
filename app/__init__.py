903af3ab8cd6df0208f0e56e021dbd5bf645f112
TheOafidian <tim.van.rillaer@hotmail.com>
2022-01-27 14:51:00 +0100

    Part 10 tutorial completed: added email support trough reset password feature

--- app/__init__.py
+++ app/__init__.py
@@ -6,6 +6,7 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+from flask_mail import Mail
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -15,6 +16,8 @@ login.login_view = 'login'
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+
+mail = Mail(app)
 
 if not app.debug:
     if app.config['MAIL_SERVER']:
@@ -38,7 +41,7 @@ if not app.debug:
     file_handler = RotatingFileHandler('logs/grocery.log', maxBytes=10240,
                                         backupCount=10)
     file_handler.setFormatter(logging.Formatter(
-        '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(line)d]'
+        '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'
     ))
     file_handler.setLevel(logging.INFO)
     app.logger.addHandler(file_handler)
